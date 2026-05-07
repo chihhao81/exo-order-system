b9ab88737d3c00a11194a2f6cece3caa1910c6fa
IrvingChang <irc@ogloba.com>
2026-05-07 13:49:55 +0800

    更新銀行帳號

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 export const BANK_ACCOUNTS = [
     // { id: 'A', label: 'Chen', bankName: '中國信託', bankCode: '822', accountNumber: '808540401057' },
     { id: 'B', label: '少鈞', bankName: '國泰世華', bankCode: '013', accountNumber: '699513716269' },
-    { id: 'C', label: '鈞媽', bankName: '中國信託', bankCode: '822', accountNumber: '0000107531864731' },
+    // { id: 'C', label: '鈞媽', bankName: '中國信託', bankCode: '822', accountNumber: '0000107531864731' },
     { id: 'D', label: '傑', bankName: '新光銀行', bankCode: '103', accountNumber: '0338501170734' },
     // { id: 'E', label: '郁幃', bankName: '玉山銀行', bankCode: '808', accountNumber: '0370979223448' },
-    { id: 'F', label: '浩誠', bankName: '玉山銀行', bankCode: '808', accountNumber: '1296979068937' },
+    { id: 'F', label: '浩誠', bankName: '元大銀行', bankCode: '806', accountNumber: '20262700098191' },
     { id: 'X', label: 'LinePay', bankName: 'LinePay', bankCode: '', accountNumber: '' },
     { id: 'Z', label: '賣貨便', bankName: '賣貨便', bankCode: '', accountNumber: '' },
 ];
